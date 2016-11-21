a1934eed0c66206439a26c4c6111d4c266195540
Jonathon Imperiosi <jimper@users.noreply.github.com>
2016-11-21 11:03:49 -0800

    [PHP] DCM API samples for v2.7
    Copied from v2.6. Updated to use new client library version (dev-master required for now). Also adding API version info to the page title, for convenience.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=139806500

---
+++ php/v2.7/examples/Misc/GetChangeLogsForAdvertiser.php
@@ -0,0 +1,105 @@
+<?php
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Require the base class.
+require_once dirname(__DIR__) . '/BaseExample.php';
+
+/**
+ * This example displays the change logs of a specified advertiser object.
+ *
+ * A similar pattern can be applied to get change logs for many other object
+ * types.
+ */
+class GetChangeLogsForAdvertiser extends BaseExample {
+  /**
+   * (non-PHPdoc)
+   * @see BaseExample::getInputParameters()
+   * @return array
+   */
+  protected function getInputParameters() {
+    return array(
+        array('name' => 'user_profile_id',
+              'display' => 'User Profile ID',
+              'required' => true),
+        array('name' => 'advertiser_id',
+              'display' => 'Advertiser ID',
+              'required' => true)
+    );
+  }
+
+  /**
+   * (non-PHPdoc)
+   * @see BaseExample::run()
+   */
+  public function run() {
+    $values = $this->formValues;
+
+    printf(
+        '<h2>Listing all change logs for advertiser ID %s</h2>',
+        $values['advertiser_id']
+    );
+
+    $response = null;
+    $pageToken = null;
+
+    $this->printResultsTableHeader('Change Logs');
+
+    do {
+      // Create and execute the change logs list request.
+      $response = $this->service->changeLogs->listChangeLogs(
+          $values['user_profile_id'],
+          array(
+              'objectIds' => array($values['advertiser_id']),
+              'objectType' => 'OBJECT_ADVERTISER',
+              'pageToken' => $pageToken
+          )
+      );
+
+      foreach ($response->getChangeLogs() as $log) {
+        $this->printResultsTableRow($log);
+      }
+
+      // Update the next page token.
+      $pageToken = $response->getNextPageToken();
+    } while(!empty($response->getChangeLogs()) && !empty($pageToken));
+
+    $this->printResultsTableFooter();
+  }
+
+  /**
+   * (non-PHPdoc)
+   * @see BaseExample::getName()
+   * @return string
+   */
+  public function getName() {
+    return 'Get All Change Logs For Advertiser';
+  }
+
+  /**
+   * (non-PHPdoc)
+   * @see BaseExample::getResultsTableHeaders()
+   * @return array
+   */
+  public function getResultsTableHeaders() {
+    return array(
+        'action' => 'Action',
+        'fieldName' => 'Field',
+        'oldValue' => 'Old Value',
+        'newValue' => 'New Value'
+    );
+  }
+}
---
+++ php/v2.7/examples/Reports/CreateFloodlightReport.php
@@ -0,0 +1,126 @@
+<?php
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Require the base class.
+require_once dirname(__DIR__) . '/BaseExample.php';
+
+/**
+ * This example creates a simple Floodlight report for the given Floodlight
+ * Configuration ID.
+ */
+class CreateFloodlightReport extends BaseExample {
+  /**
+   * {@inheritdoc}
+   * @see BaseExample::getInputParameters()
+   * @return array
+   */
+  protected function getInputParameters() {
+    return array(
+        array('name' => 'user_profile_id',
+              'display' => 'User Profile ID',
+              'required' => true),
+        array('name' => 'floodlight_config_id',
+              'display' => 'Floodlight Configuration ID',
+              'required' => true),
+        array('name' => 'start_date',
+              'display' => 'Start Date (yyyy-MM-dd)',
+              'required' => true),
+        array('name' => 'end_date',
+              'display' => 'End Date (yyyy-MM-dd)',
+              'required' => true)
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   * @see BaseExample::run()
+   */
+  public function run() {
+    $values = $this->formValues;
+
+    printf(
+        '<h2>Creating a new Floodlight report for Floodlight config ID %s</h2>',
+        $values['floodlight_config_id']
+    );
+
+    // Create a report.
+    $report = new Google_Service_Dfareporting_Report();
+    $report->setName('API Floodlight Report: Floodlight ID ' .
+        $values['floodlight_config_id']);
+    $report->setFileName('api_floodlight_report_files');
+    $report->setType('FLOODLIGHT');
+
+    // Create Floodlight criteria for the report.
+    $dateRange = new Google_Service_Dfareporting_DateRange();
+    $dateRange->setStartDate($values['start_date']);
+    $dateRange->setEndDate($values['end_date']);
+
+    $floodlightConfigDimension =
+        new Google_Service_Dfareporting_SortedDimension();
+    $floodlightConfigDimension->setName('dfa:floodlightConfigId');
+    $activityDimension = new Google_Service_Dfareporting_SortedDimension();
+    $activityDimension->setName('dfa:activity');
+    $advDimension = new Google_Service_Dfareporting_SortedDimension();
+    $advDimension->setName('dfa:advertiser');
+
+    $filter = new Google_Service_Dfareporting_DimensionValue();
+    $filter->setDimensionName('dfa:floodlightConfigId');
+    $filter->setMatchType('EXACT');
+    $filter->setValue($values['floodlight_config_id']);
+
+    $criteria = new Google_Service_Dfareporting_ReportFloodlightCriteria();
+    $criteria->setDateRange($dateRange);
+    $criteria->setDimensions(array($floodlightConfigDimension,
+        $activityDimension, $advDimension));
+    $criteria->setMetricNames(
+        array(
+            'dfa:activityClickThroughConversions',
+            'dfa:activityClickThroughRevenue',
+            'dfa:activityViewThroughConversions',
+            'dfa:activityViewThroughRevenue'
+        )
+    );
+    $criteria->setDimensionFilters(array($filter));
+
+    $report->setFloodlightCriteria($criteria);
+
+    $result = $this->service->reports->insert($values['user_profile_id'],
+        $report);
+    $this->printResultsTable('Floodlight Report', array($result));
+  }
+
+  /**
+   * {@inheritdoc}
+   * @see BaseExample::getName()
+   * @return string
+   */
+  public function getName() {
+    return 'Create Floodlight Report';
+  }
+
+  /**
+   * {@inheritdoc}
+   * @see BaseExample::getResultsTableHeaders()
+   * @return array
+   */
+  public function getResultsTableHeaders() {
+    return array(
+        'id' => 'Report ID',
+        'name' => 'Report Name'
+    );
+  }
+}
