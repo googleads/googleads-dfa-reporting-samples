d31bbbee4be7db80c5c149457aac7f49d0913248
Jon Imperiosi <jimper@rodete-desktop-imager.corp.google.com>
2018-04-03 21:57:45 -0700

    [Ruby] Adding end-to-end reporting samples to the DCM API sample set
    Ported from the Java samples @ https://github.com/googleads/googleads-dfa-reporting-samples/tree/master/java/v3.0/src/main/java/com/google/api/services/samples/dfareporting/guides. These will be used to add inline Ruby examples to the reporting guides.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=191545888

--- ruby/v3.0/reports/create_report.rb
+++ ruby/v3.0/reports/create_report.rb
@@ -16,37 +16,116 @@
 #           See the License for the specific language governing permissions and
 #           limitations under the License.
 #
-# This example illustrates how to create a report
+# An end-to-end example of how to create and configure a standard report.
 
 require_relative '../dfareporting_utils'
+require 'date'
 
 def create_report(profile_id)
   # Authenticate and initialize API service.
   service = DfareportingUtils.get_service()
 
-  # Create a new report resource to insert.
+  # 1. Create a report resource.
+  report = create_report_resource()
+
+  # 2. Define report criteria.
+  define_report_criteria(report)
+
+  # 3. (optional) Look up compatible fields.
+  find_compatible_fields(service, profile_id, report)
+
+  # 4. Add dimension filers to the report criteria.
+  add_dimension_filters(service, profile_id, report)
+
+  # 5. Save the report resource.
+  insert_report_resource(service, profile_id, report)
+end
+
+def create_report_resource()
   report = DfareportingUtils::API_NAMESPACE::Report.new({
+    # Set the required fields "name" and "type".
     :name => 'Example Standard Report',
     :type => 'STANDARD',
-    :criteria => DfareportingUtils::API_NAMESPACE::Report::Criteria.new({
-      :date_range => DfareportingUtils::API_NAMESPACE::DateRange.new({
-        :relative_date_range => 'YESTERDAY'
-      }),
-      :dimensions => [
-        DfareportingUtils::API_NAMESPACE::SortedDimension.new({
-          :name => 'dfa:campaign'
-        })
-      ],
-      :metric_names => ['dfa:clicks']
-    })
+    # Set optional fields.
+    :file_name => 'example_report',
+    :format => 'CSV'
   })
 
-  # Insert the report.
+  puts 'Creating %s report resource with name "%s".' %
+      [report.type, report.name]
+
+  return report
+end
+
+def define_report_criteria(report)
+  # Define a date range to report on. This example uses explicit start and end
+  # dates to mimic the "LAST_30_DAYS" relative date range.
+  start_date = DateTime.now.prev_day(30).strftime('%Y-%m-%d')
+  end_date = DateTime.now.strftime('%Y-%m-%d')
+
+  # Create a report criteria
+  criteria = DfareportingUtils::API_NAMESPACE::Report::Criteria.new({
+    :date_range => DfareportingUtils::API_NAMESPACE::DateRange.new({
+      :start_date => start_date,
+      :end_date => end_date
+    }),
+    :dimensions => [
+      DfareportingUtils::API_NAMESPACE::SortedDimension.new({
+        :name => 'dfa:advertiser'
+      })
+    ],
+    :metric_names => ['dfa:clicks', 'dfa:impressions']
+  })
+
+  # Add the criteria to the report resource.
+  report.criteria = criteria
+
+  puts "\nAdded report criteria:\n%s" % criteria.to_json
+end
+
+def find_compatible_fields(service, profile_id, report)
+  fields = service.query_report_compatible_field(profile_id, report)
+
+  report_fields = fields.report_compatible_fields
+
+  if report_fields.dimensions.any?
+    # Add a compatible dimension to the report.
+    report.criteria.dimensions <<
+        DfareportingUtils::API_NAMESPACE::SortedDimension.new({
+          :name => report_fields.dimensions.first.name
+        })
+  elsif report_fields.metrics.any?
+    # Add a compatible metric to the report.
+    report.criteria.metric_names << report_fields.metrics.first.name
+  end
+
+  puts "\nUpdated report criteria (with compatible fields):\n%s" %
+      report.criteria.to_json
+end
+
+def add_dimension_filters(service, profile_id, report)
+  # Query advertiser dimension values for report run dates.
+  dimension = DfareportingUtils::API_NAMESPACE::DimensionValueRequest.new({
+    :dimension_name => 'dfa:advertiser',
+    :start_date => report.criteria.date_range.start_date,
+    :end_date => report.criteria.date_range.end_date
+  })
+
+  values = service.query_dimension_value(profile_id, dimension)
+
+  unless values.items.empty?
+    # Add a value as a filter to the report criteria.
+    report.criteria.dimension_filters = [values.items.first]
+  end
+
+  puts "\nUpdated report criteria (with valid dimension filters):\n%s" %
+      report.criteria.to_json
+end
+
+def insert_report_resource(service, profile_id, report)
   report = service.insert_report(profile_id, report)
 
-  # Display results.
-  puts 'Standard report with ID %d and name "%s" was created.' %
-      [report.id, report.name]
+  puts "\nSuccessfully inserted new report with ID %s." % report.id
 end
 
 if __FILE__ == $0
